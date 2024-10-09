8ccff7fc99a98b3ce7ff4e842c999500ca16a1f8
Konstantin Makeev <kostia1500@gmail.com>
2024-10-09 20:24:59 +0300

    Improve args values logic.

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -4,14 +4,13 @@ import pytest
 from django.urls import reverse
 from pytest_lazyfixture import lazy_fixture
 
-COMMENT_ID_FOR_ARGS = lazy_fixture('comment_id_for_args')
-NEWS_ID_FOR_ARGS = lazy_fixture('news_id_for_args')
+COMMENT_FOR_ARGS = lazy_fixture('comment')
+NEWS_FOR_ARGS = lazy_fixture('news')
 ANONYMOUS_CLIENT = lazy_fixture('anonymous_client')
 AUTHOR_CLIENT = lazy_fixture('author_client')
 NOT_AUTHOR_CLIENT = lazy_fixture('not_author_client')
 
 
-@pytest.mark.django_db
 @pytest.mark.parametrize(
     'name, args, client, expected_status',
     (
@@ -20,17 +19,17 @@ NOT_AUTHOR_CLIENT = lazy_fixture('not_author_client')
         ('users:login', None, ANONYMOUS_CLIENT, HTTPStatus.OK),
         ('users:logout', None, ANONYMOUS_CLIENT, HTTPStatus.OK),
         ('users:signup', None, ANONYMOUS_CLIENT, HTTPStatus.OK),
-        ('news:detail', NEWS_ID_FOR_ARGS, ANONYMOUS_CLIENT, HTTPStatus.OK),
+        ('news:detail', NEWS_FOR_ARGS, ANONYMOUS_CLIENT, HTTPStatus.OK),
         # Страницы для авторизованных пользователей
-        ('news:edit', COMMENT_ID_FOR_ARGS, AUTHOR_CLIENT, HTTPStatus.OK),
-        ('news:delete', COMMENT_ID_FOR_ARGS, AUTHOR_CLIENT, HTTPStatus.OK),
+        ('news:edit', COMMENT_FOR_ARGS, AUTHOR_CLIENT, HTTPStatus.OK),
+        ('news:delete', COMMENT_FOR_ARGS, AUTHOR_CLIENT, HTTPStatus.OK),
         ('news:edit',
-         COMMENT_ID_FOR_ARGS,
+         COMMENT_FOR_ARGS,
          NOT_AUTHOR_CLIENT,
          HTTPStatus.NOT_FOUND
          ),
         ('news:delete',
-         COMMENT_ID_FOR_ARGS,
+         COMMENT_FOR_ARGS,
          NOT_AUTHOR_CLIENT,
          HTTPStatus.NOT_FOUND
          ),
@@ -38,21 +37,23 @@ NOT_AUTHOR_CLIENT = lazy_fixture('not_author_client')
 )
 def test_page_status_availability(client, args, name, expected_status):
     """Проверка статус кодов доступных страниц для разных пользователей."""
+    if args:
+        args = (args.id, )
     url = reverse(name, args=args)
     response = client.get(url)
     assert response.status_code == expected_status
 
 
 @pytest.mark.parametrize(
-    'name, args, client',
+    'name, client',
     (
-        ('news:edit', COMMENT_ID_FOR_ARGS, ANONYMOUS_CLIENT),
-        ('news:delete', COMMENT_ID_FOR_ARGS, ANONYMOUS_CLIENT),
+        ('news:edit', ANONYMOUS_CLIENT),
+        ('news:delete', ANONYMOUS_CLIENT),
     ),
 )
-def test_redirects(client, name, args, login_url):
+def test_redirects(client, name, login_url, comment):
     """Проверка редиректа для анонимного пользователя."""
-    url = reverse(name, args=args)
+    url = reverse(name, args=(comment.id, ))
     expected_url = f'{login_url}?next={url}'
     response = client.get(url)
     assert response.status_code == 302
